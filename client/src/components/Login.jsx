dd81444df337f3128fb0394fb702fc46e5cd66f3
Gagan M Shetty <139095188+Gagan-09@users.noreply.github.com>
2024-08-05 11:57:42 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3001/auth/login", {
+      const response = await axios.post("https://recipe-craft.vercel.app/auth/login", {
         username,
         password,
       });
