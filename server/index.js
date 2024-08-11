55167b10f6e0a68e92ebc4c6e2eee1ea8f8b57d1
Gagan M Shetty <139095188+Gagan-09@users.noreply.github.com>
2024-08-11 12:07:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://recipecraft.netlify.app/"],
+    origin: ["https://recipecraft.netlify.app"],
     methods: ["POST", "GET", "PUT"],
     credentials: true,
   })
